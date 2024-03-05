52fff018b006182332c4fd321e9b41c0ac404ba2
SpaceCore (Matthew Terpening) <145371453+SpaceCore-SCHOOL@users.noreply.github.com>
2024-03-05 12:45:49 -0500

    completed problem 1

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your maximum number? ");
+            int maxNum = int.Parse(Console.ReadLine());
+            for (int i = 0;
+             i <= maxNum;
+                i = i + 2)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
